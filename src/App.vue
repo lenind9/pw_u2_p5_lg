125bbc97d1ef3f6a8ea816fc30ad4d8cd3d0be89
“lenind9” <lenindgc@hotmail.com>
2024-01-03 19:54:23 -0500

    Componente, computed y props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="WelcomeAAAA to Your Vue.js App"/>
-  <HelloWorld msg="HOLA"/>
-  <HelloWorld msg="MUNDO"/>
+  <Contador titulo="Contador1222"/>
+  <Contador titulo="Contador2"/>
+  <Contador titulo="Contador3"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Contador from './components/Contador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  Contador
   }
 }
 </script>
---
+++ src/components/Contador.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>{{ titulo }}</h2>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+  <p>{{counter}} <sup>2</sup> = {{obtenerCuadradoComputado}}</p>
+</template>
+
+<script>
+export default {
+    props:['titulo'],
+    data() {
+        return{
+            counter: 5
+        };
+    },
+    methods:{
+        obtenerCuadrado(){
+            console.log("Se calcula el cuadrado");
+            return this.counter * this.counter;
+        }
+    },
+    computed:{
+        obtenerCuadradoComputado(){
+            console.log("Se calcula el cuadrado computado");
+            return this.counter * this.counter;
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
