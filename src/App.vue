4a0fab235c18320d2cc9ab362921c796e6282d56
Lenin Guananga <lenindgc@hotmail.com>
2024-01-18 20:50:59 -0500

    Corrección examen 1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,120 @@
 <template>
-  <PokemonPage/>
+  <!--<PokemonPage/>-->
+  <div class="container" v-if="mostrarJuego">
+    <h1>Casino Pokémon</h1>
+    <h2>Puntaje: {{ puntaje }}</h2>
+    <h2>Intento: {{ intento }}</h2>
+    <ImagenExamen :texto="texto1" :urlImg="url1" />
+    <ImagenExamen :texto="texto2" :urlImg="url2" />
+    <ImagenExamen :texto="texto3" :urlImg="url3" />
+
+    <button @click="jugar">JUGAR</button>
+  </div>
+
+  <div v-if="mostrarPerdedor">
+    <h1>Has utilizado tus 5 intentos</h1>
+    <h1>El juego ha termindo, intentalo nuevamente</h1>
+    <button @click="reiniciar">Nuevo Juego</button>
+  </div>
+
+  <div v-if="mostrarGanador">
+    <h1>Puntaje: {{ puntaje }}</h1>
+    <h1>Felicitaciones has ganado un premio de $10.000,00</h1>
+    <button @click="reiniciar">Nuevo Juego</button>
+  </div>
 </template>
 
 <script>
-import PokemonPage from './pages/PokemonPage.vue';
+//import PokemonPage from './pages/PokemonPage.vue';
+import ImagenExamen from "./components/ImagenExamen.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    PokemonPage
+    //PokemonPage
+    ImagenExamen,
   },
-}
+  data() {
+    return {
+      puntaje: 0,
+      intento: 0,
+      url1: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+      url2: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+      url3: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+      texto1: "XXXXXXXXX",
+      texto2: "XXXXXXXXX",
+      texto3: "XXXXXXXXX",
+      mostrarGanador: false,
+      mostrarPerdedor: false,
+      mostrarJuego: true,
+    };
+  },
+  methods: {
+    async jugar() {
+      // instancia 1
+      const data1 = await this.consumirAPI();
+      this.texto1 = data1.answer;
+      this.url1 = data1.image;
+
+      // instancia 2
+      const data2 = await this.consumirAPI();
+      this.texto2 = data2.answer;
+      this.url2 = data2.image;
+
+      // instancia 1
+      const data3 = await this.consumirAPI();
+      this.texto3 = data3.answer;
+      this.url3 = data3.image;
+
+      this.evaluarResultado();
+    },
+    async consumirAPI() {
+      return await fetch("https://yesno.wtf/api").then((res) => res.json());
+    },
+    evaluarResultado() {
+      this.intento++;
+      if (this.texto1 === "yes" && this.texto2 === "yes" && this.texto3 === "yes") {
+        this.puntaje += 5;
+      } else if ((this.texto1 === "yes" && this.texto2 === "yes") ||
+        (this.texto2 === "yes" && this.texto3 === "yes") ||
+        (this.texto1 === "yes" && this.texto3 === "yes")
+      ) {
+        this.puntaje += 2;
+      } else if (this.texto1 === "yes" || this.texto2 === "yes" || this.texto3 === "yes"
+      ) {
+        this.puntaje += 1;
+      }
+
+      if (this.puntaje >= 10) {
+        this.mostrarGanador = true;
+        this.mostrarPerdedor = false;
+        this.mostrarJuego = false;
+      }
+
+      if (this.intento === 5) {
+        this.mostrarPerdedor = true;
+        this.mostrarGanador = false;
+        this.mostrarJuego = false;
+      }
+    },
+    reiniciar() {
+      this.puntaje = 0;
+      this.intento = 0;
+      this.url1 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg";
+      this.url2 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg";
+      this.url3 =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg";
+      this.texto1 = "XXXXXXXXX";
+      this.texto2 = "XXXXXXXXX";
+      this.texto3 = "XXXXXXXXX";
+      this.mostrarGanador = false;
+      this.mostrarPerdedor = false;
+      this.mostrarJuego = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,5 +125,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  grid-template-columns: repeat(4, 200px);
+}
+
+h1 {
+  grid-column: span 4;
+}
+h2{
+  grid-column: span 2;
 }
 </style>
---
+++ src/components/ImagenExamen.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <img :src="urlImg" alt="No se puede visualizar" />
+    <label for="">{{texto}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String,
+    },
+    urlImg: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+img{
+  width: 200px;
+  height: 300px;
+}
+</style>
